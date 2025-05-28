8a0fce15969be8f33cc628c7fee364c3abf1d2e9
Zafir <zafir.nasim@gmail.com>
2025-05-28 16:02:06 -0500

    switch session management from express-session to cookie-session

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const session = require("express-session");
+const session = require("cookie-session");
 const sharedSession = require("express-socket.io-session");
 const { v4: uuidv4 } = require('uuid');
 
